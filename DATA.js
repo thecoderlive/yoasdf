f40a1b06769c32445e54f6f6ac2709cbe51fee93
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-07-29 11:22:41 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "8e0a9669-76d2-4a2d-9d96-550b420138fc",
+    "home_feed": [
+        {
+            "id": "22ef5bbf-9ba5-4f13-99b3-5b0edbe15e58",
+            "artist_name": "response",
+            "home_carousel": [
+                {
+                    "id": "15896962-40b3-4226-aa57-d0fbf6f39375",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png"
+                },
+                {
+                    "id": "8234686c-0bb5-4df1-a0ae-e434c83e03c2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "bb0332fb-6453-4482-9441-4a35e2733809",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "e6109b2a-c76f-47b1-ae56-283826f0cb48",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "4b0905fa-8c44-4d28-9373-be76d2249d96",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/265.png"
+                }
+            ],
+            "art_caption": "key",
+            "art_description": "Blue control many prepare return military do culture. Whatever thought cultural adult feeling television become option. From adult whom deal use want nor."
+        },
+        {
+            "id": "83baae37-350b-484b-b388-c40c781ed851",
+            "artist_name": "each",
+            "home_carousel": [
+                {
+                    "id": "eb1a5199-141b-4294-acb5-b621d1ac0882",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "1b8a4607-1d20-4e74-b167-c7824e8e1ebd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/21.png"
+                },
+                {
+                    "id": "3f00c4e0-3cc3-4dff-a696-93217bdcf50a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "fa0b75e7-5da5-450c-9783-e8758c377026",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/76.png"
+                },
+                {
+                    "id": "16f29be7-f83f-4114-bf3d-bbfad84982c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                }
+            ],
+            "art_caption": "industry",
+            "art_description": "Write quickly beat quickly must picture. Pattern institution short road authority."
+        },
+        {
+            "id": "963f5d1d-c6f4-46e3-bff6-586daefc31f3",
+            "artist_name": "second",
+            "home_carousel": [
+                {
+                    "id": "7f2ae26c-1e54-41b3-aafa-51be6e36f5e8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/284.png"
+                },
+                {
+                    "id": "8040077b-a359-4cd8-a922-369296b5c9f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/287.png"
+                },
+                {
+                    "id": "833d0f4f-741d-46d9-81ea-8e84818d1417",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                },
+                {
+                    "id": "38ae0979-9e91-4adf-82d2-390e3873beaf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                },
+                {
+                    "id": "7cc36e2f-03c9-42c6-9627-7c72e5771907",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png"
+                }
+            ],
+            "art_caption": "agent",
+            "art_description": "Here listen marriage perform although. Hear describe evidence. Argue benefit certain establish. Western during theory record."
+        },
+        {
+            "id": "50f6b480-0fbb-4187-967c-edfba8549221",
+            "artist_name": "will",
+            "home_carousel": [
+                {
+                    "id": "a810f8c4-1cd3-41cb-8043-2da0918e3348",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "b1ce6866-b882-45fe-ad68-31c9ee4ac0f5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/330.png"
+                },
+                {
+                    "id": "f7b585e6-40a7-4546-958e-51abd53f64dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/272.png"
+                },
+                {
+                    "id": "9b4dc734-e300-4868-8d75-c201b6b0e4a3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "0b6ffec7-eccc-4591-98c1-a9d4345248dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                }
+            ],
+            "art_caption": "politics",
+            "art_description": "Win even or bank onto strategy impact trouble. Office wonder help without rule. Apply ability concern model."
+        },
+        {
+            "id": "f4542a20-28fe-4187-9244-c5e79f86e60f",
+            "artist_name": "table",
+            "home_carousel": [
+                {
+                    "id": "57122ef2-bd4d-4ad6-80e8-b48a53396706",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/328.png"
+                },
+                {
+                    "id": "d6f3e43e-8ef3-45ea-8959-c7e4d7151011",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                },
+                {
+                    "id": "3da02a5e-67e6-441f-99cb-921935cce4e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "69a5f35c-662a-4be8-b42a-88a560c950b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "2966d9e9-76d7-424d-afac-cda8edd86b8e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/89.png"
+                }
+            ],
+            "art_caption": "he",
+            "art_description": "Effort work himself operation little. Only wife everyone phone production move. Necessary second notice board."
+        }
+    ]
+}
